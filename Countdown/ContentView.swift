f3e5c66f5c718ef8c37a10e7f40449646a26df4a
k-guest <k.guest.fr@gmail.com>
2023-11-21 16:51:46 +0100

    Countdown V2

--- Countdown/ContentView.swift
+++ Countdown/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Countdown
-//
-//  Created by Kevin Guest on 20/11/2023.
-//
-
 import SwiftUI
 
 struct Event: Identifiable {
@@ -12,13 +5,34 @@ struct Event: Identifiable {
     var name: String
     var color: Color
     var date: Date
-    var selectedIcon: String
+    var selectedIconURL: URL
 }
 
 class EventsViewModel: ObservableObject {
     @Published var events: [Event] = []
 
-    let availableIcons = ["star", "birthday.cake", "airplane", "movieclapper", "music.mic"]
+//    let availableIconsURLs = [
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/10353/10353827.png?ga=GA1.1.1490056685.1700553608")!,
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/723/723955.png?ga=GA1.1.1490056685.1700553608")!,
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/565/565415.png?ga=GA1.1.1490056685.1700553608")!,
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/4240/4240150.png?ga=GA1.1.1490056685.1700553608")!,
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/876/876817.png?ga=GA1.1.1490056685.1700553608")!,
+//        URL(string: "https://cdn-icons-png.flaticon.com/512/254/254477.png?ga=GA1.1.1490056685.1700553608")!
+//    ]
+
+    let availableIconsURLs = [
+        URL(string: "https://cdn-icons-png.flaticon.com/512/2052/2052723.png?ga=GA1.1.1490056685.1700553608")!,
+        URL(string: "https://cdn-icons-png.flaticon.com/512/1350/1350222.png?ga=GA1.1.1490056685.1700553608")!,
+        URL(string: "https://cdn-icons-png.flaticon.com/512/2454/2454297.png?ga=GA1.1.1490056685.1700553608")!,
+        URL(string: "https://cdn-icons-png.flaticon.com/512/3418/3418886.png?ga=GA1.1.1490056685.1700553608")!,
+        URL(string: "https://cdn-icons-png.flaticon.com/512/564/564046.png?ga=GA1.1.1490056685.1700553608")!,
+        URL(string: "https://cdn-icons-png.flaticon.com/512/867/867319.png?ga=GA1.1.1490056685.1700553608")!
+    ]
+
+    
+    var sortedEvents: [Event] {
+        return events.sorted { $0.date < $1.date }
+    }
 
     func updateEvent(event: Event) {
         if let index = events.firstIndex(where: { $0.id == event.id }) {
@@ -33,26 +47,32 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(eventsViewModel.events) { event in
+                ForEach(eventsViewModel.sortedEvents) { event in
                     NavigationLink(
                         destination: EventDetail(event: event, eventsViewModel: eventsViewModel),
                         label: {
                             EventRow(eventsViewModel: eventsViewModel, event: event)
                         }
                     )
+                    .padding(.trailing)
+                    .background(event.color)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationBarItems(leading:
                 HStack {
-                    Text("Events")
+                    Text("Événements")
                         .font(.system(size: 32))
                         .bold()
-                    Spacer()
                 },
                 trailing: NavigationLink(destination: AddEventView(eventsViewModel: eventsViewModel)) {
                     Image(systemName: "plus")
-                        .foregroundColor(.black)
-                        .font(.system(size: 28))
+                        .padding([.vertical], 8)
+                        .padding([.horizontal], 18)
+                        .background(Color(.black))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .font(.system(size: 20))
                         .bold()
                 }
             )
@@ -66,33 +86,39 @@ struct AddEventView: View {
     @State private var eventName = ""
     @State private var selectedColor: Color = .blue
     @State private var eventDate = Date()
-    @State private var selectedIcon: String = "star"
+    @State private var selectedIconURL: URL = EventsViewModel().availableIconsURLs.first!
 
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         Form {
-            Section(header: Text("Create Event")) {
-                TextField("Event Name", text: $eventName)
-                ColorPicker("Event Color", selection: $selectedColor)
-                DatePicker("Event Date", selection: $eventDate, displayedComponents: .date)
-                Picker("Event Icon", selection: $selectedIcon) {
-                    ForEach(eventsViewModel.availableIcons, id: \.self) { icon in
-                        Image(systemName: icon)
-                            .tag(icon)
+            Section(header: Text("Créer un événement")) {
+                TextField("Nom de l'événement", text: $eventName)
+                ColorPicker("Couleur de l'événement", selection: $selectedColor)
+                DatePicker("Date de l'événement", selection: $eventDate, displayedComponents: .date)
+                Picker("Icône de l'événement", selection: $selectedIconURL) {
+                    ForEach(eventsViewModel.availableIconsURLs, id: \.self) { iconURL in
+                        AsyncImage(url: iconURL) { image in
+                            image
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            
+                        } placeholder: {
+                            Text("")
+                        }
                     }
-                }
+                }.pickerStyle(.navigationLink)
             }
 
             Section {
-                Button("Add Event") {
-                    let newEvent = Event(name: eventName, color: selectedColor, date: eventDate, selectedIcon: selectedIcon)
-                    eventsViewModel.events.append(newEvent)
-                    presentationMode.wrappedValue.dismiss()
+                Button("Ajouter") {
+                    let newEvent = Event(name: eventName, color: selectedColor, date: eventDate, selectedIconURL: selectedIconURL)
+                        eventsViewModel.events.append(newEvent)
+                        presentationMode.wrappedValue.dismiss()
                 }
             }
         }
-        .navigationBarTitle("Add Event")
+        .navigationBarTitle("Ajouter un événement")
     }
 }
 
@@ -102,19 +128,18 @@ struct EventRow: View {
 
     var body: some View {
         HStack {
-            Image(systemName: event.selectedIcon)
-                .foregroundColor(.black)
-                .font(.system(size: 18))
+            Image(systemName: "photo")
+                .load(url: event.selectedIconURL)
+                .frame(width: 30, height: 30)
+                .padding(.leading)
             Text(event.name)
-                .font(.system(size: 20))
             Spacer()
             VStack {
                 Text("\(daysRemaining(for: event.date))")
                     .foregroundColor(.gray)
-                    .font(.system(size: 18))
-                Text("Days remaining")
+                Text("Jours Restants")
                     .foregroundColor(.gray)
-            }
+            }.padding(10)
         }
     }
 
@@ -130,7 +155,7 @@ struct EventDetail: View {
     @State private var editedEventName: String
     @State private var editedColor: Color
     @State private var editedDate: Date
-    @State private var editedIcon: String
+    @State private var editedIconURL: URL
 
     @State private var isEditing = false
 
@@ -143,60 +168,86 @@ struct EventDetail: View {
         _editedEventName = State(initialValue: event.name)
         _editedColor = State(initialValue: event.color)
         _editedDate = State(initialValue: event.date)
-        _editedIcon = State(initialValue: event.selectedIcon)
+        _editedIconURL = State(initialValue: event.selectedIconURL)
     }
 
     var body: some View {
-        VStack {
-            if isEditing {
-                Form {
-                    Section(header: Text("Update Event")) {
-                        TextField("Event Name", text: $editedEventName)
-                            .font(.largeTitle)
-                            .padding()
-                        ColorPicker("Event Color", selection: $editedColor)
-                        DatePicker("Event Date", selection: $editedDate, displayedComponents: .date)
-                        Picker("Event Icon", selection: $editedIcon) {
-                            ForEach(eventsViewModel.availableIcons, id: \.self) { icon in
-                                Image(systemName: icon)
-                                    .tag(icon)
+        if isEditing {
+            Form {
+                Section(header: Text("Modifier un événement")) {
+                    TextField("Nom de l'événement", text: $editedEventName)
+                        .font(.largeTitle)
+                        .padding()
+                    ColorPicker("Couleur de l'événement", selection: $editedColor)
+                    DatePicker("Date de l'événement", selection: $editedDate, displayedComponents: .date)
+                    Picker("Icône de l'événement", selection: $editedIconURL) {
+                        ForEach(eventsViewModel.availableIconsURLs, id: \.self) { iconURL in
+                            AsyncImage(url: iconURL) { image in
+                                image
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                            } placeholder: {
+                                Text("Icône")
                             }
                         }
-                    }
-                }
-            } else {
-                List {
-                    Section(header: Text("Event Detail")) {
-                        Text(editedEventName)
-                            .font(.largeTitle)
-                            .padding()
-                        Text("Date : \(formattedDate(for: editedDate))")
-                            .foregroundColor(.gray)
-                    }
+                    }.pickerStyle(.navigationLink)
+                }.navigationBarTitle("Modification de l'événement")
+                Section {
+                    Button("Sauvegarder") {
+                        let editedEvent = Event(id: event.id, name: editedEventName, color: editedColor, date: editedDate, selectedIconURL: editedIconURL)
+                        eventsViewModel.updateEvent(event: editedEvent)
+                        isEditing.toggle()
+                    }.foregroundColor(.green)
                 }
             }
-
-            Spacer()
-
-            if !isEditing {
-                Button("Edit") {
-                    isEditing.toggle()
+        } else {
+            Form {
+                Section(header: Text("Détails de l'événement")) {
+                    Text(editedEventName)
+                        .font(.largeTitle)
+                    Text("Date : \(formattedDate(for: editedDate))")
+                        .foregroundColor(.gray)
                 }
-            } else {
-                Button("Save") {
-                    let editedEvent = Event(id: event.id, name: editedEventName, color: editedColor, date: editedDate, selectedIcon: editedIcon)
-                    eventsViewModel.updateEvent(event: editedEvent)
-                    isEditing.toggle()
+                Section {
+                    Button("Modifier") {
+                        isEditing.toggle()
+                    }.foregroundColor(.blue)
+                    Button("Supprimer") {
+                        if let index = eventsViewModel.events.firstIndex(where: { $0.id == event.id }) {
+                            eventsViewModel.events.remove(at: index)
+                        }
+                    }.foregroundColor(.red)
                 }
             }
+            .navigationBarTitle(Text(editedEventName), displayMode: .inline)
         }
-        .navigationBarTitle(Text(editedEventName), displayMode: .inline)
     }
 
     private func formattedDate(for date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .long
-        return dateFormatter.string(from: date)
+
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+}
+
+extension Image {
+    func load(url: URL) -> some View {
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            case .failure:
+                Image(systemName: "photo")
+            @unknown default:
+                Image(systemName: "photo")
+            }
+        }
     }
 }
 
@@ -205,4 +256,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
